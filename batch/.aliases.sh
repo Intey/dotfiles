6674ae47349d006e96b3208a4a594586589cd9ad
Churin Dmitry <churin@10205.ru>
2017-11-16 19:59:30 +0300

    [up] alias for apt policy

--- batch/.aliases.sh
+++ batch/.aliases.sh
@@ -8,6 +8,8 @@ alias aptf="apt-file"
 alias aptfl="apt-file list"
 alias aptfs="apt-file search"
 
+alias aptp="apt-cache policy"
+
 alias mkdir='mkdir -p -v'
 alias less='/usr/share/vim/vim74/macros/less.sh'
 alias xev="xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'"

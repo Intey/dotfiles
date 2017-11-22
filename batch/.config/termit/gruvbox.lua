b4cda96b4d58deee7dae1f4b2d1a0e9e65e328b2
Churin Dmitry <churin@10205.ru>
2017-11-22 19:58:50 +0300

    [up] found termit(some bug, hehehe); restore build config for build helper

--- batch/.config/termit/gruvbox.lua
+++ batch/.config/termit/gruvbox.lua
@@ -35,21 +35,43 @@ gruvbox = {
 }
 
 -- WTF, termit break theme, after some time. What a hell?
+-- Ok. Can fix, with reset color scheme
 gruvbox2 = {
   _clr.black,   -- background
   _clr.red,     -- diff error, state delete, untracked
   _clr.bgreen,  -- diff add, correct command, 777 dir bg
   _clr.yellow,  -- pwd path
   _clr.ligth,   -- 777 dir foreground, bash astericks expansion(zsh)
-  '#000000',    -- ?
+  '#FFFFFF',
   '#FFFFFF',    --
   _clr.light,   -- file, cmd param, default, foreground
   _clr.light,   -- file, cmd param, default, foreground
   _clr.purple,  -- error input, git dirty
-  _clr.blue,    -- time, brackets, link target, executable file
+  _clr.red,    -- time, brackets, link target, executable file, npm error, git dirty
   _clr.byellow, -- host, user, arrow
   _clr.green,   -- ? directory, branch
   _clr.green,   -- ? directory, branch
   _clr.orange,  -- cmd count, symlink
-  _clr.orange   -- cmd count, symlink
+  _clr.orange,   -- cmd count, symlink
+}
+
+-- need for reseting escape sequences bugs in termit, after which colors
+-- intersects
+reset = {
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
+  _clr.black,
 }
